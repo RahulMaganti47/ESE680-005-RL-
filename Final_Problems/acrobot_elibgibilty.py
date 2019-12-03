c1eb5ddb9e14da979cd75d844ddd8bb1204a295c
Ravi Maganti <RahulMaganti@huntsman-ve703-11945.apn.wlan.private.upenn.edu>
2019-12-03 11:59:08 -0500

    changes

--- Final_Problems/acrobot_elibgibilty.py
+++ Final_Problems/acrobot_elibgibilty.py
@@ -34,6 +34,16 @@ class Policy(nn.Module):
         action = m.sample()
         return action.item() - 1, m.log_prob(action)
 
+    def select_torque(self, state):
+        rand_var = random.uniform(0, 1)
+        if (rand_var > .5):
+            action = -1 
+        else: 
+            action = 
+             
+        return action
+
+
 def compute_rewards(rewards, gamma):
     discounted_rewards = np.zeros(len(rewards))
     moving_add = 0
@@ -63,7 +73,8 @@ class Critic(nn.Module):
         if done: I = I * gamma 
         return td_error
     """
-        
+    
+
 def train(n_episodes, policy, critic, gamma, print_every=4):
     optimizer = optim.Adam(policy.parameters(), lr=0.01)
     optimizer_v = optim.Adam(critic.parameters(), lr=.05)
@@ -76,58 +87,53 @@ def train(n_episodes, policy, critic, gamma, print_every=4):
         values = [] 
         state = env.reset()
         score = 0 
-        I = 1.0  
         done = False
         
-        while not done:
+        while not done: 
+            #action = policy.select_torque(state)
             action, log_prob = policy.act(state)
             value = critic.forward(state) 
             values.append(value) 
             traj_log_probs.append(log_prob)
             next_state, reward, done, _ = env.step(action) 
-            #value_next = critic(next_state) 
-            #td_error = critic.td_error(reward, value_next, value, gamma, done, I) 
             score += reward 
-            rewards.append(reward)
+            rewards.append(reward) 
+            #env.render() 
 
             state = next_state 
 
-        #env.render() 
         scores_deque.append(score)  
         total_rewards.append(score)
             
         disc_rewards = compute_rewards(rewards, gamma)
-        disc_rewards = torch.tensor([disc_rewards]).float() 
+        disc_rewards = torch.tensor([disc_rewards]).float()  
     
-        values = torch.cat(values)
-        #values = torch.tensor([values]).float()
-        #value_loss = F.l1_loss(disc_rewards, values)
-        
+        values = torch.cat(values) 
+        # advantage: discounted_rewards - function approximated values 
         advantage = disc_rewards - values
     
         policy_loss = []  
         for log_prob in traj_log_probs:
-            policy_loss.append(-log_prob * advantage)  
+            policy_loss.append(-log_prob * advantage) 
+         
         policy_loss = torch.cat(policy_loss).sum() 
-        
+    
         
         #value_loss = Variable(value_loss, requires_grad = True)
+        # MSE loss 
         value_loss_1 = advantage.pow(2).mean()
         value_loss_1 = Variable(value_loss_1, requires_grad = True)
 
-        #print(value_loss.size()) 
-        #print(policy_loss.size())
-        # zero gradients from previous iteration
-        #total_loss = torch.stack([policy_loss, value_loss_1]).sum()
-
+        #create one loss function for actor and critic
+        total_loss = torch.stack([policy_loss, value_loss_1]).sum()
+       
+        # zero gradients from previous iteration 
         optimizer.zero_grad()
-        optimizer_v.zero_grad() 
-        policy_loss.backward() 
-        value_loss_1.backward() 
+        total_loss.backward() 
         optimizer.step()
-        optimizer_v.step() 
 
-        #add gradient trace 
+        #add gradient trace
+        
         #critic.lambd = torch.tensor(critic.lambd)
         #for p in critic.parameters(): 
         #    p.grad = torch.tensor(p.grad)
@@ -153,18 +159,17 @@ if __name__ == "__main__":
     print('observation space:', env.observation_space)
     print('action space:', env.action_space)
 
-    num_episodes = 500
-    gamma = .99
+    num_episodes = 500 
+    gamma = .99 
     policy = Policy()
     critic = Critic() 
-    total_rewards = train(num_episodes, policy, critic, gamma)
-
-    #num_episodes = np.linspace(1, num_episodes, 4)
-    #assert (len(num_episodes) == len(total_rewards)) 
-    
-    # Plot 
-    plt.plot(range(len(total_rewards)), total_rewards, 'b-')
+    total_rewards = train(num_episodes, policy, critic, gamma) 
+     
+    # Plotting  
+    plt.plot(range(len(total_rewards)), total_rewards, 'b-', label="training curve")
+    plt.axhline(max(total_rewards), 0, len(total_rewards), '-b', label="maximum average reward") 
+    plt.legend(loc="upper left")  
     plt.title('Episodes vs. Cumulative Rewards')
-    plt.xlabel('Episode')
-    plt.ylabel('Rewards')
+    plt.xlabel('Episode #')
+    plt.ylabel('Cumulative Reward')
     plt.show()
